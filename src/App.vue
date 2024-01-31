1f1fa4f1506c8dd9332dee5642aab64ff032a1f9
Renan Rossi Dias <renan.dias@inflor.com.br>
2024-01-31 19:48:32 -0300

    adiciona exemplos de componentização na aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <BotaoPrincipal :texto="'Botao 1'" />
+    <BotaoPrincipal :texto="'Não clique aqui'" />
+    <BotaoPrincipal corDoBotao="blue" corDoTexto="white"/>
+    <BotaoPrincipal corDoBotao="pink" />
+    <BotaoPrincipal corDoBotao="#a8a632" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import BotaoPrincipal from "./components/BotaoPrincipal.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    BotaoPrincipal
   }
-}
+};
 </script>
 
 <style>
---
+++ src/components/BotaoPrincipal.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="botao" :style="{ backgroundColor: corDoBotao, color: corDoTexto }" @click="clicou()">{{ texto }}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        texto: {
+            type: String,
+            default: 'Clique aqui',
+        },
+        corDoBotao: {
+            type: String,
+            default: 'white',
+        },
+        corDoTexto: {
+            type: String,
+            default: 'black',
+        }
+    },
+};
+</script>
+
+<style>
+.botao {
+    font-size: 16px;
+    height: 30px;
+}
+</style>
